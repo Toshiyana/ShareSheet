e7c4d1ff5a27ec5df034f949bca49390c7640925
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2021-11-22 16:10:53 +0900

    add files

--- ShareSheet/ViewController.swift
+++ ShareSheet/ViewController.swift
@@ -8,10 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .link
+        button.setTitle("Tap me", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        button.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
+        button.center = view.center
+        button.addTarget(self, action: #selector(presentShareSheet(_:)), for: .touchUpInside)
+        view.addSubview(button)
+    }
+    
+    @objc private func presentShareSheet(_ sender: UIButton) {
+        guard let image = UIImage(systemName: "bell"), let url = URL(string: "https://www.google.com") else {
+            return
+        }
+
+//        let shareSheetVC = UIActivityViewController(activityItems: [], applicationActivities: nil)
+
+        let shareSheetVC = UIActivityViewController(
+            activityItems: [
+                image,// save imageを選択すると、ベルの画像がアルバムに保存される
+                url // リンクのコピーを選択すると、urlがコピーされる
+            ],
+            applicationActivities: nil
+        )
+        
+        // iPad Support
+        shareSheetVC.popoverPresentationController?.sourceView = sender
+        shareSheetVC.popoverPresentationController?.sourceRect = sender.frame
+        
+        present(shareSheetVC, animated: true, completion: nil)
     }
 
 
